6410aeeb1eb7a36bca9c9f7ca8d662e68e9a5c76
Nitin Nayar <119853191+nnayar-r2c@users.noreply.github.com>
2023-09-12 13:10:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-import logging
-import logging.handlers
-import os
-
 import requests
+import sys
+import json
+import re
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+SEMGREP_APP_TOKEN = "XXXX"
+FILTER_IMPORTANT_FINDINGS = True
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+def get_deployments():
+    headers = {"Accept": "application/json", "Authorization": "Bearer " + SEMGREP_APP_TOKEN}
+
+    r = requests.get('https://semgrep.dev/api/v1/deployments',headers=headers)
+    if r.status_code != 200:
+        sys.exit(f'Get failed: {r.text}')
+    data = json.loads(r.text)
+    slug_name = data['deployments'][0].get('slug')
+    print("Accessing org: " + slug_name)
+    return slug_name
+
+def get_projects(slug_name):
+    
+    headers = {"Accept": "application/json", "Authorization": "Bearer " + SEMGREP_APP_TOKEN}
+
+    r = requests.get('https://semgrep.dev/api/v1/deployments/' + slug_name + '/projects?page=0',headers=headers)
+    if r.status_code != 200:
+        sys.exit(f'Get failed: {r.text}')
+    data = json.loads(r.text)
+    for project in data['projects']:
+        project_name = project['name']
+        print("Getting findings for: " + project_name)
+        get_findings_per_repo(slug_name, project_name)
+
+
+def get_findings_per_repo(slug_name, repo):
+      
+    headers = {"Accept": "application/json", "Authorization": "Bearer " + SEMGREP_APP_TOKEN}
+
+    r = requests.get('https://semgrep.dev/api/v1/deployments/' + slug_name + '/findings?repos='+repo,headers=headers)
+    if r.status_code != 200:
+        sys.exit(f'Get failed: {r.text}')
+    data = json.loads(r.text)
+    file_path = re.sub(r"[^\w\s]", "", repo) + ".json"
+    if FILTER_IMPORTANT_FINDINGS == True:
+        data = [obj for obj in data['findings'] if obj["severity"] == "high" and obj["confidence"] == "high" or obj["confidence"] == "medium"]
+    with open(file_path, "w") as file:
+         json.dump(data, file)
 
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+    slug_name = get_deployments()
+    get_projects(slug_name)
